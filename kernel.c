11fc0b6de9243ab9fa8477c1110cf4b53d4c0efe
Dinuda <ydinuda@gmail.com>
2021-04-05 16:20:31 +0530

    add colour configuration

---
+++ kernel.c
@@ -0,0 +1,10 @@
+// assigns the start address
+// resets terminal location
+#define VGA_ADDRESS 0xB8000 /*video memory begins here */
+
+/* VGA provides support for 16 colours */
+#define BLACK 0 
+#define GREEN 2
+#define RED 4
+#define YELLOW 14
+#define WHITE_COLOR 15 
