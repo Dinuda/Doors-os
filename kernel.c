5c2ea351735f2e58d49652c353e4884087165547
Dinuda <ydinuda@gmail.com>
2021-04-05 18:58:55 +0530

    add terminal buffer

--- kernel.c
+++ kernel.c
@@ -8,3 +8,30 @@
 #define RED 4
 #define YELLOW 14
 #define WHITE_COLOR 15 
+
+unsigned short *terminal_buffer;
+unsigned int vga_index;
+
+
+/* clears the terminal */
+void clear_screen(void)
+{
+    int index = 0;
+    /* there are 25 lines each of 80 coloumns each element takes 2 bytes*/
+    while (index < 80 * 25 * 2) {
+        terminal_buffer[index] = ' ';
+        index += 2;
+    }
+}
+
+// prints the message in one color
+void print_string(char *str, unsigned char color)
+{
+    int index = 0;
+    while (str[index]) {
+        terminal_buffer[vga_index] = (unsigned short)str[index]|(unsigned short)color << 8;
+        index++;
+        vga_index++;
+    }
+}
+
